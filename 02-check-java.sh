fd9a6aab639b8d7220a59db4a95104fe0ce1e67d
Dmitry Zakharov <zakharov@ie.ibm.com>
2026-06-23 15:08:48 +0100

    no docker for local
    Signed-off-by: Dmitry Zakharov <zakharov@ie.ibm.com>

--- 02-check-java.sh
+++
@@ -1,8 +0,0 @@
-docker exec spark bash -x -c "
-   cd /home/iceberg/local && 
-   java \
-      --add-opens java.base/sun.nio.ch=ALL-UNNAMED \
-      --add-opens java.base/java.nio=ALL-UNNAMED \
-      -Dlog4j.configurationFile=log4j2.properties \
-      -cp \"/opt/spark/jars/*\" src/main/java/icb/CheckSpark.java
-   "
